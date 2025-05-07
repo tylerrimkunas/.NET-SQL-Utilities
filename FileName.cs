8cdb46e68ea53d43e9b016b804a5636bc5265d8d
Tyler <tylerrimkunas@gmail.com>
2025-05-06 21:50:59 -0400

    Added main file.

---
+++ FileName.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace SQLUtility
+{
+    internal class FileName
+    {
+        public void main()
+        {
+            var query = "SELECT * FROM Users";
+            var con = new SqlConnection(query);
+            using ( SqlCommand cmd = con.CreateCommand())
+            {
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
